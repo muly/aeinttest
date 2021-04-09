8b4d9bfb8c9a3d1e18ea253c22785127b7d8bed1
Srinivasa R Muly <msrinivasareddy@gmail.com>
2021-04-09 03:14:59 -0400

    Update aeunittest_test.go

--- aeunittest_test.go
+++ aeunittest_test.go
@@ -7,13 +7,12 @@ import (
 func Test1(t *testing.T) {
 	tcs := TestCases{}
 
-	err := tcs.Load(`test\lifelog test cases - Goal.csv`, ',', true)
+	err := tcs.Load(`test/lifelog test cases - Goal.csv`, ',', true)
 	if err != nil {
 		t.Fatal(err)
 	}
 
 	for _, tc := range tcs {
-		tc.Run()
+		tc.RunCheckStatusCode()
 	}
-
 }
